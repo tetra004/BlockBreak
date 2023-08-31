fd5e18160d67429e914df3a46d1d3298dc5ab58a
Quu <91326783+Quu23@users.noreply.github.com>
2023-08-31 11:46:32 +0900

    gameLoopを定義した

--- package/Main.java
+++ package/Main.java
@@ -1,5 +1,26 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
 public class Main{
+
     public static void main(String[] args) {
         Window win = new Window("Test");
+
+        Timer timer = new Timer(0, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameLoop();
+                win.cv.repaint();
+            }
+        });
+
+        timer.start();
     }
+
+    public static void gameLoop(){
+        //gameLoopの中身書いてね
+    }
+
 }
