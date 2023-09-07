291934039f6f4e0a2461e0a814b1b923491b1ce9
ryota <mike.maro0810@gamail.com>
2023-09-07 09:25:42 +0900

    Playerを動くようにした

--- package/Player.java
+++ package/Player.java
@@ -1,7 +1,13 @@
 public class Player extends Entity{
     
+    final int SPEED = 1;
+
     Player(double x,double y,int width, int height){
         super(x, y, width, height);
     }
     
+    public void move(){
+        if(Window.isKeyPressed[0]&&this.x>0)this.x-=this.SPEED;
+        if(Window.isKeyPressed[1]&&this.x<Window.WIDTH)this.x+=SPEED;
+    }
 }
