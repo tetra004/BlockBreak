66bf5c0340e4143ab97eb1a85839e7a9ab901a8d
Quu <91326783+Quu23@users.noreply.github.com>
2023-09-07 09:07:42 +0900

    key入力を受け取れるようにした

--- package/Window.java
+++ package/Window.java
@@ -1,6 +1,12 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 
 public class Window extends JFrame{
+
+    /** [0]:A [1]:D */
+    public static boolean[] isKeyPressed = {false,false};
 
     Canvas cv;
 
@@ -15,5 +21,40 @@ public class Window extends JFrame{
 
         this.cv = new Canvas(this.getWidth(), this.getHeight());
         this.add(this.cv);
+
+        this.addKeyListener(new KeyListener(){
+
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case 'A':
+                        isKeyPressed[0]=true;
+                        break;
+                    case 'D':
+                        isKeyPressed[1]=true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case 'A':
+                        isKeyPressed[0]=false;
+                        break;
+                    case 'D':
+                        isKeyPressed[1]=false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        });
     }
 }
