1f8cfe2401e6d50d488a63f6aae401c4e350bca2
Quu <91326783+Quu23@users.noreply.github.com>
2023-09-08 09:46:14 +0900

    blockはいれつを生成するメゾット作った

--- package/Block.java
+++ package/Block.java
@@ -1,10 +1,24 @@
 public class Block extends Entity{
     
     int score;
+    static final int WIDTH = 80;
+    static final int HEIGHT= 100;
 
     Block(double x,double y,int score){
-        super(x, y, 80, 100);
+        super(x, y, Block.WIDTH, Block.HEIGHT);
         score = this.score;
     }
-    //ブロックの座標は左上
+
+    public static Block[][] generateArrays(){
+
+        Block[][] blocks = new Block[3][5];
+
+        for(int i = 0;i<blocks.length;i++){
+            for(int j = 0;j<blocks[i].length;j++){
+                blocks[i][j] = new Block(50+Block.WIDTH*j, -10-Block.HEIGHT*i , 10);
+            }
+        }
+
+        return blocks;
+    }
 }
--- package/Main.java
+++ package/Main.java
@@ -4,18 +4,10 @@ import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
 public class Main{
-    static Block blocks[][] = new Block[3][5];
+    static Block blocks[][] = Block.generateArrays();
 
     public static void main(String[] args) {
         Window win = new Window("Test");
-
-        final int WIDTH = 80;
-        final int HEIGHT = 100;
-        for(int i = 0;i<blocks.length;i++){
-            for(int j = 0;j<blocks[i].length;j++){
-                blocks[i][j] = new Block(50+WIDTH*j, -10-HEIGHT*i, WIDTH, HEIGHT, 10);
-            }
-        }
 
         Timer timer = new Timer(0, new ActionListener() {
             @Override
